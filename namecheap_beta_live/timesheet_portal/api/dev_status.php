7538e442a65f4affd15d4dbc1d06a8336e24b954
leopardinstinct-web <242228113+leopardinstinct-web@users.noreply.github.com>
2026-08-26 13:12:34 +0500

    Track client administration tables in DEV diagnostics

--- namecheap_beta_live/timesheet_portal/api/dev_status.php
+++ namecheap_beta_live/timesheet_portal/api/dev_status.php
@@ -14,6 +14,7 @@ try {
     $serverVersion = (string)$pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
 
     $tables = [
+        'clients', 'client_role_authority', 'dev_client_preferences', 'dashboard_layouts',
         'employees', 'stores', 'employee_store_access', 'employee_store_assignments',
         'employee_hourly_rate_history', 'store_weekly_hours', 'store_shift_start_times',
         'employee_logs', 'attendance_shifts', 'attendance_disputes',
